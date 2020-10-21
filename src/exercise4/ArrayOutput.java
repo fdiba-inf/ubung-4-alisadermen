f59afd4a3d14660730321c46a0dec3ab4fd24b48
alisadermen <alisadermen@gmail.com>
2020-10-21 14:09:45 +0000

    ubung 4

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -7,13 +7,15 @@ public class ArrayOutput {
     System.out.println("Enter array size: ");
     int a = input.nextInt();
     int[] numbers = new int[a];
+    String space = " ";
     System.out.println("Enter array elements: ");
     for (int index = 0; index < numbers.length; index++) {
       numbers[index] = input.nextInt();
      }
     for (int number: numbers) {
       System.out.println(number);
-      System.out.print(" ");
+      System.out.print(space);
+      space = space + " ";
     }
     
     }
---
+++ src/exercise4/ArrayReversal.java
@@ -0,0 +1,13 @@
+package exercise4;
+import java.util.Arrays;
+import java.util.Scanner;
+public class ArrayReversal {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter array size: ");
+    int a = input.nextInt();
+    double[] numbers = new double[a];
+    System.out.println("Enter array elements: ");
+    for (int i = ; i > numbers.length; i-- ) {
+      System.out.print
+    }
